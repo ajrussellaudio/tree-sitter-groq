b981847560f6bacdc044ada4a413d1d838994741
Alan Russell <ajrussellaudio@gmail.com>
2025-07-16 18:43:33 +0100

    Update parser

--- src/parser.c
+++ src/parser.c
@@ -6914,8 +6914,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_groq(void) {
     .name = "groq",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
+      .major_version = 1,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
